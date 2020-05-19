e61c1e51281443f05c8634b63d936295e884ba7c
GabrielWinter <59836341+GabrielWinter@users.noreply.github.com>
2020-05-19 10:14:40 -0300

    exemplo1.py

---
+++ exemplo1.py
@@ -0,0 +1,12 @@
+
+def  externa ( func ):
+    def  interna ( x , y ):
+        return  func ( x , y )
+    return  interna
+
+
+@ externa
+def  soma ( x , y ):
+    return  x  +  y
+
+print ( soma ( 2 , 2 ))
